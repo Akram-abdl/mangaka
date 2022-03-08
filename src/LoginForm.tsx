8d4fb17c5660d8cde3e112683e6b7f4b9b2e8dc5
Akram <akram.abdl@outlook.fr>
2022-03-08 12:57:21 +0100

    started authentification

---
+++ src/LoginForm.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function LoginForm() {
+    return (
+        <div className="LoginForm">
+            <p>salut</p>
+        </div>
+    );
+}
+
+export default LoginForm;
--- src/authentification.tsx
+++ src/authentification.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import LoginForm from './LoginForm';
+
 import reportWebVitals from './reportWebVitals';
 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <LoginForm />
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
