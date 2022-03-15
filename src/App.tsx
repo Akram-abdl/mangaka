a81bfa904da48a2c72b774152715d70197f4b56e
Akram <akram.abdl@outlook.fr>
2022-03-15 10:46:37 +0100

    basic login firebase with gmail

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import LoginPage from './pages/Login/LoginComponent';
+
+import AuthRoute from './component/AuthRoute';
+import HomePage from './pages/Home/HomeComponent';
+import config from './utils/firebaseSetup';
+import { initializeApp } from 'firebase/app';
+
+initializeApp(config.firebaseConfig);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+            <Routes>
+                <Route
+                    path="/"
+                    element={
+                        <AuthRoute>
+                            <HomePage />
+                        </AuthRoute>
+                    }
+                />
+                <Route path="/login" element={<LoginPage />} />
+            </Routes>
+        </BrowserRouter>
   );
 }
 
--- src/utils/firebaseSetup.ts
+++ src/utils/firebaseSetup.ts
@@ -1,21 +1,28 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: "AIzaSyAuwrAggKVb8dO7tiSN6hNZ40saYsKLjFE",
-    authDomain: "mangakafb.firebaseapp.com",
-    projectId: "mangakafb",
-    storageBucket: "mangakafb.appspot.com",
-    messagingSenderId: "736146979950",
-    appId: "1:736146979950:web:f665e7305e26bc9ee13a7f",
-    measurementId: "G-MGMPXD6STV"
+const config = {
+
+    firebaseConfig: {
+        apiKey: "AIzaSyAuwrAggKVb8dO7tiSN6hNZ40saYsKLjFE",
+        authDomain: "mangakafb.firebaseapp.com",
+        projectId: "mangakafb",
+        storageBucket: "mangakafb.appspot.com",
+        messagingSenderId: "736146979950",
+        appId: "1:736146979950:web:f665e7305e26bc9ee13a7f",
+        measurementId: "G-MGMPXD6STV"
+
+
+    }
+  
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+export default config;
